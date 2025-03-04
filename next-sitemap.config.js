ab4c5bc2611ed00eb075c538f98436cdcb3b92c5
adas <ajanta744@gmail.com>
2025-03-04 12:05:11 +1100

    update sitemap confige file

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://webdeveloperinmelbourne.com.au', 
+  siteUrl: 'https://www.webdeveloperinmelbourne.com.au', 
   generateRobotsTxt: true, 
   sitemapSize: 5000, 
   generateIndexSitemap: false, 
